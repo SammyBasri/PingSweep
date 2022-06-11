e1fc102f4e067996d4bc5416c7ba6446d11bf06f
SammyBasri <89367242+SammyBasri@users.noreply.github.com>
2022-06-10 20:12:00 -0400

    Update ping-sweep.sh

--- ping-sweep.sh
+++ ping-sweep.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Usage ./ping-sweep.sh <First three octets of you your IP> <First 4th octet> <Last 4th octet>
+
 for i in $(eval echo {$2..$3})
 
 do ping -c 1 $1.$i | grep "64 bytes" |  cut -d " " -f  4 | cut -d ":" -f  1 
